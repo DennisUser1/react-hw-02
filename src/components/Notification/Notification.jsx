a961fe930a16304bf6ff6c50fee9801feca8bfe9
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-09-13 13:58:13 +0300

    v.1.3. Fixed styles

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,7 +2,7 @@ import styles from "./Notification.module.css";
 
 const Notification = () => (
   <div className={styles.container}>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28"><path fill="#492002" fill-rule="nonzero" d="M14 2c6.627 0 12 5.373 12 12s-5.373 12-12 12S2 20.627 2 14 7.373 2 14 2Zm0 9a1 1 0 0 0-1 1v8a1 1 0 1 0 2 0v-8a1 1 0 0 0-1-1Zm0-4.25a1.25 1.25 0 1 0 0 2.5 1.25 1.25 0 0 0 0-2.5Z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28"><path fill="#492002" fillRule="nonzero" d="M14 2c6.627 0 12 5.373 12 12s-5.373 12-12 12S2 20.627 2 14 7.373 2 14 2Zm0 9a1 1 0 0 0-1 1v8a1 1 0 1 0 2 0v-8a1 1 0 0 0-1-1Zm0-4.25a1.25 1.25 0 1 0 0 2.5 1.25 1.25 0 0 0 0-2.5Z"/></svg>
     <p className={styles.message}>Feedback has not been provided yet.</p>
   </div>
 );
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,11 +1,16 @@
 import styles from "./Options.module.css";
 
+const handleClick = (event) => {
+  console.log('Button clicked'); 
+  event.currentTarget.blur(); 
+};
+
 const Options = ({ updateFeedback, resetFeedback, totalFeedback }) => (
   <div className={styles.container}>
-    <button onClick={() => updateFeedback("good")}>Good</button>
-    <button onClick={() => updateFeedback("neutral")}>Neutral</button>
-    <button onClick={() => updateFeedback("bad")}>Bad</button>
-    {totalFeedback > 0 ? <button onClick={resetFeedback}>Reset</button> : ''}
+    <button onClick={(e) => { updateFeedback("good"); handleClick(e); }}>Good</button>
+    <button onClick={(e) => { updateFeedback("neutral"); handleClick(e); }}>Neutral</button>
+    <button onClick={(e) => { updateFeedback("bad"); handleClick(e); }}>Bad</button>
+    {totalFeedback > 0 ? <button onClick={(e) => {resetFeedback(); handleClick(e); }}>Reset</button> : ''}
   </div>
 );
 
