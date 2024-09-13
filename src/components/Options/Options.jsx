c76e6fc1dc9d780cc8ff91cfa6962f62d439b455
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-09-13 15:52:24 +0300

    v.1.4. Fixed styles

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,16 +1,26 @@
 import styles from "./Options.module.css";
 
-const handleClick = (event) => {
-  console.log('Button clicked'); 
-  event.currentTarget.blur(); 
-};
+// const handleClick = (event) => {
+//   console.log('Button clicked'); 
+//   event.currentTarget.style.backgroundColor = '#9d5523';
+//   event.currentTarget.style.borderColor = 'transparent';  
+// };
+
+// const Options = ({ updateFeedback, resetFeedback, totalFeedback }) => (
+//   <div className={styles.container}>
+//     <button onClick={(e) => { updateFeedback("good"); handleClick(e); }}>Good</button>
+//     <button onClick={(e) => { updateFeedback("neutral"); handleClick(e); }}>Neutral</button>
+//     <button onClick={(e) => { updateFeedback("bad"); handleClick(e); }}>Bad</button>
+//     {totalFeedback > 0 ? <button onClick={(e) => {resetFeedback(); handleClick(e); }}>Reset</button> : ''}
+//   </div>
+// );
 
 const Options = ({ updateFeedback, resetFeedback, totalFeedback }) => (
   <div className={styles.container}>
-    <button onClick={(e) => { updateFeedback("good"); handleClick(e); }}>Good</button>
-    <button onClick={(e) => { updateFeedback("neutral"); handleClick(e); }}>Neutral</button>
-    <button onClick={(e) => { updateFeedback("bad"); handleClick(e); }}>Bad</button>
-    {totalFeedback > 0 ? <button onClick={(e) => {resetFeedback(); handleClick(e); }}>Reset</button> : ''}
+<button onClick={() => updateFeedback("good")}>Good</button>
+    <button onClick={() => updateFeedback("neutral")}>Neutral</button>
+    <button onClick={() => updateFeedback("bad")}>Bad</button>
+    {totalFeedback > 0 ? <button onClick={resetFeedback}>Reset</button> : ''}
   </div>
 );
 
