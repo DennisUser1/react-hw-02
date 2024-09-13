f600d121b2a22fa69d46d1d100f63537611a613d
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-09-13 23:30:06 +0300

    v.1.5. Fixed styles

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,12 +3,12 @@ import styles from "./Feedback.module.css";
 const Feedback = ({ good, neutral, bad, total, positive }) => (
   <div className={styles.container}>
     <h2>Feedback Statistics</h2>
-    <p>Good: {good}</p>
-    <p>Neutral: {neutral}</p>
-    <p>Bad: {bad}</p>
-    <p>Total: {total}</p>
-    <p>Positive Feedback: {positive}%</p>
+    <p>Good: <span className={styles.value}>{good}</span></p>
+    <p>Neutral: <span className={styles.value}>{neutral}</span></p>
+    <p>Bad: <span className={styles.value}>{bad}</span></p>
+    <p>Total: <span className={styles.value}>{total}</span></p>
+    <p>Positive Feedback: <span className={styles.value}>{positive}%</span></p>
   </div>
 );
-
+ 
 export default Feedback;
