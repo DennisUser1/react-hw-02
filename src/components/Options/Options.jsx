9ee83fbf70dab17130775d5fd4e80ed05ba83368
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-09-14 13:05:45 +0300

    v.1.6. Fixed styles + Readme.md file

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -17,7 +17,7 @@ import styles from "./Options.module.css";
 
 const Options = ({ updateFeedback, resetFeedback, totalFeedback }) => (
   <div className={styles.container}>
-<button onClick={() => updateFeedback("good")}>Good</button>
+    <button onClick={() => updateFeedback("good")}>Good</button>
     <button onClick={() => updateFeedback("neutral")}>Neutral</button>
     <button onClick={() => updateFeedback("bad")}>Bad</button>
     {totalFeedback > 0 ? <button onClick={resetFeedback}>Reset</button> : ''}
